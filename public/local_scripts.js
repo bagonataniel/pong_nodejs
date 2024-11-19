232ec7a902ae80b39ed7550cb6163f252a77d379
bagonataniel <bagonataniel@gmail.com>
2024-11-19 10:53:42 +0100

    fixing modules

--- public/local_scripts.js
+++ public/local_scripts.js
@@ -1,5 +1,13 @@
+const player = document.querySelector(".control");
 export function PlayerOverflowHandling() {
     console.log("hello")
+    if (player.style.position < 0) {
+        console.log("felul van")
+    }
+    if (player.style.position > 100) {
+        console.log("alul van");
+        
+    }
 };
 
 export function add(a, b) {
